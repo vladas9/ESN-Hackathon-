9abc9192650d14c0c8fc9f6e5c7d018e0b0db778
vladas9 <vladamza63@gmail.com>
2023-11-18 16:01:40 +0200

    Add news request

--- app.py
+++ app.py
@@ -6,6 +6,67 @@ app = Flask(__name__)
 
 @app.route('/')
 
+# GET most recent news with comments
+@app.route('/news', methods=['GET'])
+def get_recent_news():
+    conn = sqlite3.connect('main.db')
+    cursor = conn.cursor()
+    # Perform a LEFT JOIN to fetch the news and their comments, if any
+    cursor.execute('''
+        SELECT n.*, GROUP_CONCAT(c.comment_text) FROM news n
+        LEFT JOIN comment c ON n.id = c.news_id
+        GROUP BY n.id
+        ORDER BY n.post_date DESC
+    ''')
+    news_data = cursor.fetchall()
+    conn.close()
+
+    news_list = []
+    for news in news_data:
+        # Assuming the last column of the SELECT result is the concatenated comments
+        comments = news[-1]
+        comments_list = comments.split(',') if comments else []
+        news_list.append({
+            'party_id': news[1],
+            'post_date': news[2],
+            'theme_id': news[3],
+            'rating': news[4],
+            'media_link': news[5],
+            'comments': comments_list
+        })
+
+    return jsonify(news_list)
+
+# GET news by ID with comments
+@app.route('/news/<int:news_id>', methods=['GET'])
+def get_news_by_id(news_id):
+    conn = sqlite3.connect('main.db')
+    cursor = conn.cursor()
+    # Perform a LEFT JOIN to fetch the news item and its comments, if any
+    cursor.execute('''
+        SELECT n.*, GROUP_CONCAT(c.comment_text) FROM news n
+        LEFT JOIN comment c ON n.id = c.news_id
+        WHERE n.id = ?
+        GROUP BY n.id
+    ''', (news_id,))
+    news_item = cursor.fetchone()
+    conn.close()
+
+    if news_item:
+        comments = news_item[-1]
+        comments_list = comments.split(',') if comments else []
+        news_details = {
+            'party_id': news_item[1],
+            'post_date': news_item[2],
+            'theme_id': news_item[3],
+            'rating': news_item[4],
+            'media_link': news_item[5],
+            'comments': comments_list
+        }
+        return jsonify(news_details)
+    else:
+        return jsonify({'error': 'News item not found'}), 404
+
 # GET PARTIES
 @app.route('/parties')
 def show_parties():
