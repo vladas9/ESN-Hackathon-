81fbe75f99277a77fad0bc60dbdf6aa1332ece38
Timur Cravtov <tmrkrvtsv@gmail.com>
2023-11-18 22:01:10 +0200

    added adding?

--- add.py
+++ add.py
@@ -1,7 +1,11 @@
+import random
 import sqlite3
 from datetime import datetime
+from scrape import main
+from random import randint
 
 DATABASE = 'tesst.db'
+NR_OF_PARTIES = 2
 
 def clear_table(table_name):
     try:
@@ -47,7 +51,9 @@ def add_news(party_id, post_date, theme, rating, media_link, content):
 
 
 def add_party(party_name, party_logo, party_description, official_website):
+
     try:
+
         with sqlite3.connect(DATABASE) as conn:
             cursor = conn.cursor()
             cursor.execute('''
@@ -59,7 +65,6 @@ def add_party(party_name, party_logo, party_description, official_website):
                     official_website TEXT
                 )
             ''')
-
             cursor.execute('''
                 INSERT INTO parties (party_name, party_logo, party_description, official_website)
                 VALUES (?, ?, ?, ?)
@@ -67,16 +72,36 @@ def add_party(party_name, party_logo, party_description, official_website):
 
             conn.commit()
             print("New party successfully added to the 'parties' table")
+
+
     except sqlite3.Error as e:
         print("An error occurred while adding a party:", e)
+        conn.close()
+
+clear_table("parties")
 
 # Пример использования
-add_party('Party A', 'logo_a.png', 'Description of Party A', 'https://www.partya.com')
-add_party('Party B', 'logo_b.png', 'Description of Party B', 'https://www.partyb.com')
+add_party('MAN', 'https://ionceban.md/favicon.ico', 'The National Alternative Movement (Romanian: Mișcarea Alternativa Națională) is a political party in Moldova. It was created at the end of December 2021. The party is led by Chișinău Mayor Ion Ceban.[1]The party is in opposition to the current government of the Republic of Moldova.',
+          'https://alternativa.eu/')
+add_party('PAS', 'https://unpaspentru.md/wp-content/uploads/2020/08/fav-1-100x100.png', 'PAS este o echipă de oameni care face o altfel de politică în Republica Moldova. Noi putem construi o altfel de țară, în care se poate trăi mai bine aici, acasă.', 'https://unpaspentru.md/')
+
 
 clear_table("news")
 
-add_news(1, post_date=datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-         theme="Măsuri urgente în fata avertismentului meteorologic. Ion Ceban:”Serviciile municipale intervin în teren",
-         media_link="https://ionceban.md/wp-content/uploads/2023/11/ion_ceban_curatenia.jpg",
-         content="Hello World", rating=4)
+news = main()
+# print(news)
+
+for new in news:
+    content_all = ''
+    for paragraph in new["info"]:
+        content_all += paragraph + '\n'
+
+    add_news(random.randint(1, NR_OF_PARTIES), theme=new["title"], content=content_all, post_date=datetime.now().strftime('%Y-%m-%d %H:%M:%S'), media_link=new["images"][0], rating=random.randint(2, 9))
+
+#
+
+
+# add_news(1, post_date=datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+#          theme="Măsuri urgente în fata avertismentului meteorologic. Ion Ceban:”Serviciile municipale intervin în teren",
+#          media_link="https://ionceban.md/wp-content/uploads/2023/11/ion_ceban_curatenia.jpg",
+#          content="Hello World", rating=4)
