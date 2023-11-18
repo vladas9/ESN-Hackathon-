3f95d28754779b2bd43baf1f5625ae243111a23e
IacovlevMaxim <iacovlev.maxim@gmail.com>
2023-11-18 17:11:31 +0200

    trying to send /news req to db

--- news-front/app/(website)/page.js
+++ news-front/app/(website)/page.js
@@ -26,30 +26,31 @@ import { getAllPosts } from "@/lib/sanity/client";
  */
 
 export default async function IndexPage() {
+  const posts = await fetch('/news').then(req => req.json());
   // const posts = await getAllPosts();
-  const post = {
-    title: "Test Title",
-    publishedAt: new Date(Date.now()).toISOString(),
-    slug: {
-      current: "001"
-    },
-    mainImage: {
-      image: "https://www.tailwindtoolbox.com/products/tailus-alt.jpg",
-      blurDataURL: "https://www.tailwindtoolbox.com/products/tailus-alt.jpg"
-    },
-    author: {
-      name: "Viorel Bostan",
-      image: "https://media.licdn.com/dms/image/C4D03AQHdW_nAv9RKdw/profile-displayphoto-shrink_800_800/0/1579156231812?e=1705536000&v=beta&t=L9TZjXR3fE543IdT2NnU1xmK_zkelUcvhjJA3xoafZk",
-      //image: "https://cdn.sanity.io/images/cijrdavx/production/05951a0ec1a6ffc54f615ab160649e92fea982d0-800x764.png?rect=0,0,800,468&w=800&auto=format",
+  // const post = {
+  //   title: "Test Title",
+  //   publishedAt: new Date(Date.now()).toISOString(),
+  //   slug: {
+  //     current: "001"
+  //   },
+  //   mainImage: {
+  //     image: "https://www.tailwindtoolbox.com/products/tailus-alt.jpg",
+  //     blurDataURL: "https://www.tailwindtoolbox.com/products/tailus-alt.jpg"
+  //   },
+  //   author: {
+  //     name: "Viorel Bostan",
+  //     image: "https://media.licdn.com/dms/image/C4D03AQHdW_nAv9RKdw/profile-displayphoto-shrink_800_800/0/1579156231812?e=1705536000&v=beta&t=L9TZjXR3fE543IdT2NnU1xmK_zkelUcvhjJA3xoafZk",
+  //     //image: "https://cdn.sanity.io/images/cijrdavx/production/05951a0ec1a6ffc54f615ab160649e92fea982d0-800x764.png?rect=0,0,800,468&w=800&auto=format",
       
-      slug: {
-        current: "Viorel Bostan"
-      }
-    }
-  };
-  const posts = [
-    post, post, post, post, post, post, post, post,post, post, post, post  
-  ];
+  //     slug: {
+  //       current: "Viorel Bostan"
+  //     }
+  //   }
+  // };
+  // const posts = [
+  //   post, post, post, post, post, post, post, post,post, post, post, post  
+  // ];
   return <HomePage posts={posts} />;
 }
 
